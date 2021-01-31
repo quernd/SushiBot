775f3e4b12710f5f90f2e38d89cafc57e5824f48
Daniel Quernheim <quernd@users.noreply.github.com>
2021-01-31 17:15:21 +0100

    `boolean' instead of `bool' in table `users'

--- bot_schema.sql
+++ bot_schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users (user_id int primary key not null, authorized bool);
+CREATE TABLE users (user_id int primary key not null, authorized boolean);
 CREATE TABLE messages (message_id int primary key not null, message text);
 CREATE TABLE updates (update_id int primary key not null, message_id int);
 CREATE TABLE photos (photo_id text primary key not null);
